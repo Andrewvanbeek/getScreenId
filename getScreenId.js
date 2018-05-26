cee90765736ac74f1b5c7a82bde5fd392f0f2623
andrew van beek <andrewvanbeek90@gmail.com>
2018-05-26 19:10:02 -0400

    Update getScreenId.js

--- getScreenId.js
+++ getScreenId.js
@@ -171,7 +171,7 @@ getScreenId(function (error, sourceId, screen_constraints) {
 
             loadCallback();
         };
-        iframe.src = 'https://www.webrtc-experiment.com/getSourceId/'; // https://wwww.yourdomain.com/getScreenId.html
+        iframe.src = 'https://proliferate.herokuapp.com/getScreenId.html'; // https://wwww.yourdomain.com/getScreenId.html
         iframe.style.display = 'none';
         (document.body || document.documentElement).appendChild(iframe);
     }
